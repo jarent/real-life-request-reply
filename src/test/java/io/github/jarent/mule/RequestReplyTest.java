4758684525841257f8ee46ad92a15a75c26e1064
jarent <jarent@gmail.coom>
2015-01-13 00:30:54 -0600

    Source code for request-reply error handling article

--- src/test/java/io/github/jarent/mule/RequestReplyTest.java
+++ src/test/java/io/github/jarent/mule/RequestReplyTest.java
@@ -3,8 +3,10 @@ package io.github.jarent.mule;
 import java.util.List;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.munit.runner.functional.FunctionalMunitSuite;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 public class RequestReplyTest extends FunctionalMunitSuite {
@@ -29,31 +31,41 @@ public class RequestReplyTest extends FunctionalMunitSuite {
 	
 	@Test
 	public void shouldInovokeLongRunningProcessSequentially() throws Exception {
+			
 		
 		 runFlow("initial-process-flow" , testEvent(SAMPLE_INPUT_10_RECORDS.split(",")));	
 		
+		 verifyCallOfMessageProcessor("outbound-endpoint").ofNamespace("vm").withAttributes(ImmutableMap.of("path", (Object)"finalMessage")).times(4);
+			
 	}
 	
 	@Test
 	public void shouldInovokeLongRunningProcessConcurrently() throws Exception {
 		
-		 runFlow("request-reply-flow" , testEvent(SAMPLE_INPUT_10_RECORDS.split(",")));	
+		runFlow("request-reply-flow" , 
+				 testEvent(SAMPLE_INPUT_10_RECORDS.split(",")));	
 		
+		verifyCallOfMessageProcessor("outbound-endpoint").ofNamespace("vm").withAttributes(ImmutableMap.of("path", (Object)"finalMessage")).times(4);
+			
 	}
 	
-	@Test
+	
+	//max. time - 100s - sequentially it will take 10x more 
+	@Test(timeout=100000)
 	public void shouldFinishFastForLargeInputData() throws Exception {
 		
 		 StringBuilder sampleInput1000Records = new StringBuilder();
-		 for (int i = 0; i <= 100; ++i) {
+		 for (int i = 0; i < 100; ++i) {
 			 sampleInput1000Records.append(SAMPLE_INPUT_10_RECORDS);
-			 if (i != 100) {
+			 if (i != 99) {
 				 sampleInput1000Records.append(",");
 			 }
 		 }
 		
 		 runFlow("request-reply-flow" , testEvent(
 				 sampleInput1000Records.toString().split(",")));	
+		 
+		 verifyCallOfMessageProcessor("outbound-endpoint").ofNamespace("vm").withAttributes(ImmutableMap.of("path", (Object)"finalMessage")).times(334);
 		
 	}
 
